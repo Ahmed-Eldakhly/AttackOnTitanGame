8ed261cdc585b5fe774e0c961e74ae8dd01eb5e2
hossamradwan <hossam.radwan.engineer@gmail.com>
2021-01-06 22:43:35 +0200

    enemy-movement-oop-Hossam

--- js/enemy-movement.js
+++ js/enemy-movement.js
@@ -1,42 +1,66 @@
-// frame.style.backgroundColor = "yellow";
-// document.body.style.backgroundColor = "red";
-
 var enemy = ["image/characters/enemy-1.png","image/characters/enemy-2.png",
                 "image/characters/enemy-3.png","image/characters/enemy-4.png",
                 "image/characters/enemy-5.png","image/characters/enemy-6.png",
                 "image/characters/enemy-7.png","image/characters/enemy-8.png"];
-var i = 0;
-function myMove() { 
+
+class enemyClass{
+  constructor(images,lastPosition,speed)
+  {
+    this.images = images;
+    this.speed = speed;
+    this.lastPosition = lastPosition;
+  }
+  move()
+  {
     var box = document.getElementById("frame");
     var character = document.createElement("img");
-    character.src = enemy[0];
+
+    character.src = this.images[0];
+
     box.appendChild(character);
     character.style.position = "relative";
-    var pos = 1150;
-    character.style.left = pos + "px"; 
-    var id = setInterval(frame, 200);
+    
+    var pos = window.outerWidth;  //1150
+    character.style.left = pos + "px";
+
+    var id = setInterval(frame, this.speed);
+
+    var img = this.images;
+    var last = this.lastPosition  ;
+    var i = 0;
     function frame() 
-    {
-      if (pos <= -200) 
+    { 
+      console.log(window.outerWidth);  
+      if (pos <= -120) 
       {
         //clearInterval(id);
-        pos = 1150;
+        pos =  window.outerWidth; //1150
       } 
       else 
       {
-        character.src = enemy[i];
+        character.src = img[i];
         character.style.left = pos + "px"; 
+        pos -= 40;
         console.log(pos);
-        pos -= 140; 
         i = i + 1;
         if(i > 7)
         {
             i = 0;
-        }
-        
+        }  
       }
     }
-}
+  }
+};  
+
+enemy1 = new enemyClass(enemy,200,100);
+
+enemy1.move();
+
+
+
+
+
+
 
   
 
