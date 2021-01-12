b15900eebdad6283edeb24eb3fcccc222371e444
OmarMohamedAwad <omar.awad825@gmail.com>
2021-01-12 19:28:32 +0200

    edit level creation

--- js/level-creation.js
+++ js/level-creation.js
@@ -8,7 +8,7 @@ var enemyPhotosArray = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7
 var background1, background2;
 
 // Game Roof 
-var roofPosetionX = 0, roof;
+var roofPosetionX = -10, roof;
 
 function gameCreation(level, character) {
 
@@ -22,7 +22,7 @@ function gameCreation(level, character) {
         // Build roof
         for (let i = 0; i < 4; i++) {
             roof = new Building("roof.png", 500, 150, roofPosetionX, "0px");
-            roofPosetionX += 520;
+            roofPosetionX += 510;
         }
         // Launch enemy attack
         Enemy.launchAttack(enemies);
@@ -33,7 +33,7 @@ function gameCreation(level, character) {
                 var erenMovePhotosArray = ["eren/1.png", "eren/2.png", "eren/3.png", "eren/4.png", "eren/5.png", "eren/6.png"];
                 var erenWinPhotosArray = ["eren-win", "eren/mov3", "eren/mov4", "eren/mov5", "eren/mov6", "eren/mov7", "eren/mov1"];
                 var erenLosePhotosArray = ["eren-lose", "eren/lose1.png", "eren/lose1.png", "eren/lose2.png", "eren/lose3.png", "eren/lose3.png"];
-                mainCharacter = new Characters(character, "Eren jeager", 20, 1, erenJumpPhotosArray, erenMovePhotosArray, erenLosePhotosArray, erenWinPhotosArray, document.getElementById("defenderPhotos"));
+                mainCharacter = new Characters(character, "Eren jeager", 20, level, erenJumpPhotosArray, erenMovePhotosArray, erenLosePhotosArray, erenWinPhotosArray, document.getElementById("defenderPhotos"));
 
                 break;
             case 2:
@@ -42,7 +42,7 @@ function gameCreation(level, character) {
                 var mikasaMovePhotosArray = ["mikasa/1.png", "mikasa/2.png", "mikasa/3.png", "mikasa/4.png", "mikasa/5.png"];
                 var mikasaWinPhotosArray = ["mikasa-win", "mikasa/mov3", "mikasa/mov4", "mikasa/mov5", "mikasa/mov6", "mikasa/mov7", "mikasa/mov1"];
                 var mikasaLosePhotosArray = ["mikasa-lose", "mikasa/lose1.png", "mikasa/lose1.png", "mikasa/lose2.png", "mikasa/lose3.png", "mikasa/lose3.png"];
-                mainCharacter = new Characters(character, "Mikasa", 20, 1, mikasaJumpPhotosArray, mikasaMovePhotosArray, mikasaLosePhotosArray, mikasaWinPhotosArray, document.getElementById("defenderPhotos"));
+                mainCharacter = new Characters(character, "Mikasa", 20, level, mikasaJumpPhotosArray, mikasaMovePhotosArray, mikasaLosePhotosArray, mikasaWinPhotosArray, document.getElementById("defenderPhotos"));
                 break;
             case 3:
                 $("#defenderPhotos").attr('src', 'image/characters move/armin/1.png');
@@ -50,7 +50,7 @@ function gameCreation(level, character) {
                 var arminMovePhotosArray = ["armin/1.png", "armin/2.png", "armin/3.png", "armin/4.png", "armin/5.png"];
                 var arminWinPhotosArray = ["armin-win", "armin/mov3", "armin/mov4", "armin/mov5", "armin/mov6", "armin/mov7", "armin/mov1"];
                 var arminLosePhotosArray = ["armin-lose", "armin/lose1.png", "armin/lose1.png", "armin/lose2.png", "armin/lose3.png", "armin/lose3.png"];
-                mainCharacter = new Characters(character, "Mrmin", 20, 1, arminJumpPhotosArray, arminMovePhotosArray, arminLosePhotosArray, arminWinPhotosArray, document.getElementById("defenderPhotos"));
+                mainCharacter = new Characters(character, "Mrmin", 20, level, arminJumpPhotosArray, arminMovePhotosArray, arminLosePhotosArray, arminWinPhotosArray, document.getElementById("defenderPhotos"));
                 break;
         }
     } else if (level == 2) {
